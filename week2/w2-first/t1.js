093eff6f58f4794733775ffcc5080b71342dbb60
msimsiki <mustafasimsiki@gmail.com>
2022-01-19 12:41:32 +0200

    fix

--- week2/w2-first/t1.js
+++ week2/w2-first/t1.js
@@ -4,6 +4,6 @@ function sortNumbers()
   table1.push(document.getElementById("number1").value.toString());
   table1.push(document.getElementById("number2").value.toString());
   table1.push(document.getElementById("number3").value.toString());
-  table1 = talukko1.sort(function(a,b){return a-b});
+  table1 = table1.sort(function(a,b){return a-b});
   return document.getElementById("a1").innerHTML = table1;
 }
